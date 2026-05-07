1f6614e4afa830c3bd766a2a3adf6d78cb4d40bf
Jmnsitxo <HabidCan31@gmail.com>
2026-05-06 22:45:59 -0600

    avances

---
+++ src/main/java/com/ux/edu/conversational/ia/PromptConfig.java
@@ -0,0 +1,26 @@
+package com.ux.edu.conversational.ia;
+
+
+public class PromptConfig {
+
+    private String rol;
+
+    private String instrucciones;
+
+    private String entrada;
+
+
+
+    public PromptConfig(String rol, String instrucciones, String entrada) {
+
+        this.rol = rol;
+
+        this.instrucciones = instrucciones;
+
+        this.entrada = entrada;
+
+    }
+
+    // Getters...
+
+}
